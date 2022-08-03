0441e6b87a10c656dc69cf2aaa162fa9b0ff5294
DESKTOP-APCA3AA <rockysoft6@gmail.com>
2022-08-03 15:22:56 +0800

    Initial commit

---
+++ src/test/java/Example.java
@@ -0,0 +1,14 @@
+import com.microsoft.playwright.*;
+
+public class Example {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType chromium = playwright.chromium();
+            Browser browser = chromium.launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/java/");
+            // other actions...
+            browser.close();
+        }
+    }
+}
